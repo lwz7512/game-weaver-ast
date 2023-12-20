cd2191797ae7a79d2e0cc5542691cd072dcc3866
liwenzhi <lwz7512@gmail.com>
2023-12-19 21:04:31 -0400

    add generator for challenge content

--- docs/javascript_advanced.md
+++ docs/javascript_advanced.md
@@ -36,15 +36,25 @@ Overview ~=> Concetps ~=> Tutorials ~=> Projects
 
 ## Learning methods and mindset
 
-
 - [clean-code-javascript](https://github.com/ryanmcdermott/clean-code-javascript)
 - [The Node.js best practices list ](https://github.com/goldbergyoni/nodebestpractices)
 
 ## Explore wider world
 
-- [he Concise TypeScript Book](https://github.com/gibbok/typescript-book)
+- [The Concise TypeScript Book](https://github.com/gibbok/typescript-book)
 - [Algorithms and Data Structures implemented in JavaScript](https://github.com/TheAlgorithms/JavaScript)
 - [JavaScript Algorithms and Data Structures](https://github.com/trekhleb/javascript-algorithms)
+- [JavaScript to Know for React](https://kentcdodds.com/blog/javascript-to-know-for-react)
+
+## React
+
+- [Official Documentation](https://reactjs.org/docs/getting-started.html)
+- [Beta Official Documentation](https://beta.reactjs.org/)
+- [The Beginner's Guide to React](https://egghead.io/courses/the-beginner-s-guide-to-react)
+- [patterns.dev](https://www.patterns.dev/)
+- [React Bits](https://github.com/vasanthk/react-bits)
+- [React Patterns](https://reactpatterns.com/)
+- [Tao Of React](https://alexkondov.com/tao-of-react/)
 
 ## Developer Road map
 
---
+++ generator.js
@@ -0,0 +1,74 @@
+// TODO: challenge generate 5 content file in its folder ...
+// need folder name such as `c1` ...
+const fs = require('fs')
+
+const args = process.argv
+
+if (args.length < 3) {
+  return console.warn('## no folder name assigned!')
+}
+
+const folder = args[2]
+
+console.log(`=== generate content for folder: ${folder}`)
+
+const dir = `./challenges/${folder}`
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
+  console.log(`>>> folder: ${folder} created!`)
+}
+
+const option = { encoding: 'utf8', flag: 'r' }
+
+// read base file
+const tmplBaseFile = './challenges/cx_base.txt'
+const baseFileContent = fs.readFileSync(tmplBaseFile, option)
+// write the base file
+const targetBaseFile = `./challenges/${folder}/${folder}_base.txt`
+fs.writeFileSync(targetBaseFile, baseFileContent)
+
+const tmplSubtitleMD = './challenges/cx_subtitle.md'
+const subtitleContent = fs.readFileSync(tmplSubtitleMD, option)
+// write the subtitle
+const targetSubtitle = `./challenges/${folder}/${folder}_subtitle.md`
+fs.writeFileSync(targetSubtitle, subtitleContent)
+
+// read test json
+const tmplTestJSON = './challenges/cx_test.json'
+const testFileContent = fs.readFileSync(tmplTestJSON, option)
+
+// write test json
+const targetTestJSON = `./challenges/${folder}/${folder}_test.json`
+fs.writeFileSync(targetTestJSON, testFileContent)
+
+// write empty start & final file
+const targetStartFile = `./challenges/${folder}/${folder}_start.txt`
+const targetFinalFile = `./challenges/${folder}/${folder}_final.txt`
+fs.writeFileSync(targetStartFile, `// start code for challenge ${folder}`)
+fs.writeFileSync(targetFinalFile, `// final code for challenge ${folder}`)
+
+// challenges.json
+const challengesJSON = './data/challenges.json'
+const challengesContent = fs.readFileSync(challengesJSON, option)
+const challenges = JSON.parse(challengesContent)
+const challengeId = parseInt(folder.charAt(1))
+const challengesWithNew = challenges.map((clg) => {
+  if (clg.id === challengeId) {
+    return {
+      ...clg,
+      baseCode: `challenges/${folder}/${folder}_base.txt`,
+      finalCode: `challenges/${folder}/${folder}_final.txt`,
+      startCode: `challenges/${folder}/${folder}_start.txt`,
+      testCode: `challenges/${folder}/${folder}_test.json`,
+      videoSubtitle: `challenges/${folder}/${folder}_subtitle.md`,
+    }
+  } else {
+    return clg
+  }
+})
+
+const prettyJSON = JSON.stringify(challengesWithNew, null, 2)
+fs.writeFileSync(challengesJSON, prettyJSON)
+
+console.log(`========== challenges updated! ======`)
